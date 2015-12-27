58bd9c3397434958a30576efbe78afe141c38cc0
Emil Cardell <emil.cardell@gmail.com>
2015-12-27 01:16:08 +0100

    Added missing files

---
+++ static/js/createUserForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import iz from 'emilcardell/iz';
+import 'github/fetch';
+
+const CreateUserForm = React.createClass({
+    getInitialState() {
+        return { email: '' };
+    },
+    handleEmailChange: function(event) {
+        this.setState( { email: event.target.value } );
+    },
+    handleSubmit(e) {
+        e.preventDefault();
+
+        let validationResult = iz(this.state.email).required().email();
+        console.log(validationResult);
+
+        //validate
+        //sendToServer
+        //showMessage
+    },
+    render() {
+        return (<form onSubmit={this.handleSubmit}>
+          <div className="row">
+              <label htmlFor="exampleEmailInput">Your email</label>
+              <input className="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput" onChange={this.handleEmailChange} value={this.state.email} required />
+              <label htmlFor="exampleRecipientInput">Type of account</label>
+             <select className="u-full-width" id="exampleRecipientInput">
+               <option value="Option 1">Option 1</option>
+               <option value="Option 2">Option 2</option>
+               <option value="Option 3">Option 3</option>
+             </select>
+              <input className="button-primary u-full-width" type="submit" value="Submit" />
+          </div>
+        </form>);
+    }
+});
+
+export default CreateUserForm;
